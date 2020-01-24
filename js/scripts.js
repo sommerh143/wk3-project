bd678b7afd796dda711d40d397fecbbdd4900cfb
sh <sommerh143@yahoo.com>
2020-01-24 14:05:31 -0800

    not work try .replace and .find

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,24 @@ $(function(){
     event.preventDefault();
     var number = parseInt($("input#Entered").val());
     var test = Array(number).fill(0).map((x, y) => x + y);
+//var testone = test[i].replace("1", "Boo!");
 
     for(var i = 0; i <test.length;  i+= 1)
     {
-      if(test[i] === 1) {
-       test[i]="Boo!"
-       console.log(test);
-      } else if(test[i] === 2 ) {
-      test[i] = "Bop!";
+      test.forEach(function(test){
+        test.replace(1 , "Boo!");
       console.log(test);
-      } else if(test[i] === 3) {
-        test[i] = "sorry we cant do that.";
-      }
 
-
-    }
-    console.log(test);
-  })
+        // else if(test[i] === 2 ) {
+        //   test[i] = "Bop!";
+        //   } else if(test[i] === 3) {
+        //     test[i] = "sorry we cant do that.";
+        //   }
+      });
+      
+      } 
+     
+    
+    
+  });
 })
