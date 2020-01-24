82d2edbe6629c2f48ec0b9e11457afdb2bf05055
sh <sommerh143@yahoo.com>
2020-01-24 14:45:34 -0800

    turned the array of numbers into a string

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,19 @@ $(function(){
   $("form#inputBox").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#Entered").val());
-    var test = Array(number).fill(0).map((x, y) => x + y);
-//var testone = test[i].replace("1", "Boo!");
-
-    for(var i = 0; i <test.length;  i+= 1)
-    {
-      test.forEach(function(test){
-        test.replace(1 , "Boo!");
-      console.log(test);
-
-        // else if(test[i] === 2 ) {
-        //   test[i] = "Bop!";
-        //   } else if(test[i] === 3) {
-        //     test[i] = "sorry we cant do that.";
-        //   }
-      });
-      
-      } 
+    var test = Array(number += 1).fill(0).map((x, y) => x + y);
+    var testone = test.toString();
+    // for(var i = 0; i <test.length;  i+= 1)
+    // {
+    //      if (test[i] === 1) {
+    //    test[i]="Boo!"
+    //   } else if(test[i] === 2 ) {
+    //   test[i] = "Bop!";
+    //   } else if(test[i] === 3) {
+    //     test[i] = "sorry we cant do that.";
+    //   }
      
-    
-    
-  });
+    // }
+    console.log(testone);
+  })
 })
