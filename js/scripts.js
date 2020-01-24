1c2a0251df783b4ceef7c0f8cf1f32afbc3c46ad
sh <sommerh143@yahoo.com>
2020-01-24 11:59:57 -0800

    make array of numbers for the number that was inputed

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+
 $(function(){
   $("form#inputBox").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#Entered").val());
-    alert(number);
-    console.log(number);
+    var test = Array(number).fill(0).map((x, y) => x + y);
+
+    for(var i = 0; i <test.length;  i+= 1)
+    {
+      if(test[i] === 1) {
+      // var numberone = test.replace("1", "Boo!");
+       console.log(test);
+      }else{
+        alert("no");
+      }
+     
+    }
+    console.log(test);
   })
 })
