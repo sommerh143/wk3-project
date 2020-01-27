5a13325818790015ee72888179799aac4df223cf
Sommer Howe <sommerh143@yahoo.com>
2020-01-26 22:45:49 -0800

    find ultiples of 2 and 3 and change them to boop, bop.

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,48 @@ $(function(){
   $("form#inputBox").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#Entered").val());
-    var test = Array(number += 1).fill(0).map((x, y) => x + y);
+    
+    for (var i = 0; i <= number; i+= 1){
+      if (i % 3 == 0) {
+        if(i === /3/g){
+         console.log('Fiizz');
+        } else {
+          console.log(i);
+        }
+        
+      } else if (i % 5 == 0){
+        console.log('Buzz');
+      } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log('fizzBuzz');
+      } else {
+        console.log(i);
+      }
+    }
 
-    var num = test.join(" ");
+    // var test = Array(number += 1).fill(0).map((x, y) => x + y);
+
+
+
+
+
+
+    // var num = test.join(" ");
 
     //var test = num.replace( /1/g , "Boo!");
 
     
-      for(var i = 0; i <test.length;  i+= 1)
-    {
-         if (test[i] === 1) {
-         var num = num.replace( /1/g , "Boo!");
-      } else if(test[i] === 2 ) {
-        var num = num.replace( /2/g , "Bop!");
-      } else if(test[i] === 3) {
-        var num = num.replace( /3/g , "sorry no");
-      }
+    //   for(var i = 0; i <test.length;  i+= 1)
+    // {
+    //      if (test[i] === 1) {
+    //      var num = num.replace( /1/g , "Boo!");
+    //   } else if(test[i] === 2 ) {
+    //     var num = num.replace( /2/g , "Bop!");
+    //   } else if(test[i] === 3) {
+    //     var num = num.replace( /3/g , "sorry no");
+    //   }
      
-    }
-    console.log(num);
+    // }
+    // console.log(num);
       
   })
 })
